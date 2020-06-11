7ae62c07889cb5aafbd7c29b0904ce208ce8d944
Marquard Dirk Pienaar <mdpienaar@gmail.com>
2020-06-11 10:31:39 +0200

    Add files via upload

---
+++ Blogs/userexample/example.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta charset="utf-8">
+      <meta content="width=device-width, initial-scale=1">
+      <title>Example</title>
+      <link href="style.css" type="text/css" rel="stylesheet">
+      
+
+  </head>
+  
+  <body>
+  <?php print "<h1>In Development</h1>"; ?>
+  </body>
+</html>
